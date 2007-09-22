2c773e6d71cb3d96da8a9af4adfcd415ebdf835e
rlb3 <rlb3@3efe9002-19ed-0310-8735-a98156148065>
2007-09-22 20:43:11 +0000

    Adding MooseX::Iterator to the repo
    git-svn-id: http://code2.0beta.co.uk/moose/svn/MooseX-Iterator/trunk@2722 3efe9002-19ed-0310-8735-a98156148065

---
+++ lib/MooseX/Iterator.pm
@@ -0,0 +1,72 @@
+package MooseX::Iterator;
+use Moose;
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:RLB';
+
+extends 'Moose::Meta::Attribute';
+
+has 'provides'     => ( is => 'ro', isa => 'HashRef', default => sub { {} } );
+has 'iterate_over' => ( is => 'ro', isa => 'Str',     default => ''         );
+
+has 'methods' => (
+    is      => 'ro',
+    isa     => 'HashRef',
+    default => sub {
+        {
+            next => sub {
+                my ( $self, $collection ) = @_;
+                my $position = $self->__position;
+                my $next     = $self->$collection->[ $position++ ];
+                $self->__position($position);
+                return $next;
+              },
+              has_next => sub {
+                my ( $self, $collection ) = @_;
+                my $position = $self->__position;
+                return exists $self->$collection->[ $position++ ];
+              },
+              peek => sub {
+                my ( $self, $collection, $method_names ) = @_;
+
+                my $has_next = $method_names->{'has_next'};
+
+                if ( $self->$has_next ) {
+                    my $position = $self->__position;
+                    return $self->$collection->[ ++$position ];
+                }
+                return;
+              },
+              contents => sub {
+                my ( $self, $collection ) = @_;
+                return $self->$collection;
+              },
+        };
+    },
+);
+
+after 'install_accessors' => sub {
+    my ($attr)     = @_;
+    my $class      = $attr->associated_class;
+    my $collection = $attr->iterate_over;
+
+    $class->add_attribute(
+        '__position', => ( is => 'rw', isa => 'Int', default => 0 ) );
+
+    my %revere_methods = reverse %{ $attr->provides };
+
+    foreach my $method ( keys %{ $attr->provides } ) {
+        $class->add_method(
+            $method => sub {
+                my ($self) = @_;
+                $attr->methods->{ $attr->provides->{$method} }
+                  ->( $self, $collection, \%revere_methods );
+            },
+        );
+
+    }
+};
+
+no Moose;
+
+1;
---
+++ t/01.basic.t
@@ -0,0 +1,53 @@
+#!perl
+
+use Test::More tests => 18;
+
+package TestIterator;
+
+use Moose;
+use MooseX::Iterator;
+
+has numbers => (
+    is      => 'ro',
+    isa     => 'ArrayRef',
+    default => sub { [ 5, 6, 7, 8, 9, 10 ] },
+);
+
+has iter => (
+    metaclass    => 'MooseX::Iterator',
+    iterate_over => 'numbers',
+    provides     => {
+        my_next     => 'next',
+        my_has_next => 'has_next',
+        my_peek     => 'peek',
+        my_contents => 'contents',
+    },
+);
+
+no Moose;
+
+package main;
+
+my $test = TestIterator->new;
+
+my $count = 5;
+while ( $test->my_has_next ) {
+    my $peek = $test->my_peek;
+    my $next = $test->my_next;
+
+    is $next, $count, 'Current position value ' . $count;
+
+    if ( $count < 10 ) {
+        ok $test->my_has_next, 'has next';
+    }
+    else {
+        ok !$test->my_has_next, 'does not have next';
+        last;
+    }
+
+    is $peek, $count + 1, 'peek ahead of ' . $count . " ($peek)";
+
+    $count += 1;
+}
+
+is_deeply $test->my_contents, [ 5, 6, 7, 8, 9, 10 ], 'Contents';
