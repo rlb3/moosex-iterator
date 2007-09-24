7ff1cd8459eaf8904f637864aba569bfa1cdb4bc
rlb3 <rlb3@3efe9002-19ed-0310-8735-a98156148065>
2007-09-24 02:56:24 +0000

    Add error checking
    git-svn-id: http://code2.0beta.co.uk/moose/svn/MooseX-Iterator/trunk@2738 3efe9002-19ed-0310-8735-a98156148065

--- lib/MooseX/Iterator/Meta/Iterable.pm
+++ lib/MooseX/Iterator/Meta/Iterable.pm
@@ -2,6 +2,8 @@ package MooseX::Iterator::Meta::Iterable;
 use Moose;
 use MooseX::Iterator::Array;
 
+use Carp 'confess';
+
 our $VERSION   = '0.04';
 our $AUTHORITY = 'cpan:RLB';
 
@@ -11,6 +13,10 @@ has iterate_over => ( is => 'ro', isa => 'Str', default => '' );
 
 before '_process_options' => sub {
     my ( $class, $name, $options ) = @_;
+
+    if (defined $options->{is}) {
+        confess "Can not use 'is' with the Iterable metaclass" ;
+    }
 
     $class->meta->add_attribute( iterate_name => ( is => 'ro', isa => 'Str', default => $name ) );
 };
@@ -27,7 +33,9 @@ after 'install_accessors' => sub {
             my ($self) = @_;
             my $collection = $self->$collection_name;
 
-            if ( $self->meta->get_attribute($collection_name)->type_constraint eq 'ArrayRef' ) {
+            my $type = $self->meta->get_attribute($collection_name)->type_constraint;
+
+            if ( $type eq 'ArrayRef' ) {
                 MooseX::Iterator::Array->new( collection => $self->$collection_name );
             }
         }
